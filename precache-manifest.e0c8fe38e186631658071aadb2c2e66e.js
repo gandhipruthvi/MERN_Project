00acd6d5c169e73b44b7676c88a2f60141f4fcd4
gandhipruthvi <gandhi.pruthvi@gmail.com>
2022-04-18 20:34:07 -0400

    Updates

---
+++ precache-manifest.e0c8fe38e186631658071aadb2c2e66e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e80a4c3ca98eb4a97383",
+    "url": "/MERN_Project/static/css/main.83da74a0.chunk.css"
+  },
+  {
+    "revision": "e80a4c3ca98eb4a97383",
+    "url": "/MERN_Project/static/js/main.e80a4c3c.chunk.js"
+  },
+  {
+    "revision": "b58e368a7abeac20414d",
+    "url": "/MERN_Project/static/css/1.ecc56e02.chunk.css"
+  },
+  {
+    "revision": "b58e368a7abeac20414d",
+    "url": "/MERN_Project/static/js/1.b58e368a.chunk.js"
+  },
+  {
+    "revision": "6c9a89232bddcf66f738",
+    "url": "/MERN_Project/static/js/runtime~main.6c9a8923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MERN_Project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6638a9ed721ee70759b62dcb12ac3b4a",
+    "url": "/MERN_Project/static/media/auction.6638a9ed.jpg"
+  },
+  {
+    "revision": "15f420d46f6783391c246d2050c0f2e7",
+    "url": "/MERN_Project/static/media/auction2.15f420d4.jpg"
+  },
+  {
+    "revision": "5b96584f4ec8e23e588f54e3839a5bca",
+    "url": "/MERN_Project/static/media/auction3.5b96584f.jpg"
+  },
+  {
+    "revision": "75edb975977f18d48b5896eae9ec78b5",
+    "url": "/MERN_Project/static/media/bestbid.75edb975.jpg"
+  },
+  {
+    "revision": "8f3d7a4026a7bc307286a2196009534b",
+    "url": "/MERN_Project/static/media/ThankYou.8f3d7a40.jpg"
+  },
+  {
+    "revision": "d38ecc8fad2074f22a816755cbdc2f67",
+    "url": "/MERN_Project/index.html"
+  }
+];
